b30c5ba437ba5dd1056fe516cb3a468c9ae175ac
Laurent <laurentsingamalom@gmail.com>
2025-08-14 19:23:45 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,3 @@
-import { Sun, Moon } from "lucide-react";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { RefreshCcw, Shuffle, Copy, Download, Upload, Settings2, Info, Sparkles, Trash2, Sun, Moon } from "lucide-react";
 
